10e60865ccb89b64a90a1a20ddef64c8febadf60
Rift25 <misic.nino@gmail.com>
2020-11-21 02:31:48 -0500

    f

---
+++ Assets/New Folder/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace tabuleiro
+{
+    public enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
